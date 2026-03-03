82e2061b5e5825cf6cb41951b9e3121bd5b2667e
csc34716-coder <csc34716@gmail.com>
2026-03-03 22:26:17 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,1 +1,17 @@
+import { useState } from "react";
+import UploadBox from "../components/UploadBox";
+import ResultCard from "../components/ResultCard";
 
+function Home() {
+  const [result, setResult] = useState("");
+
+  return (
+    <div>
+      <h1>🌱 Plant AI App</h1>
+      <UploadBox setResult={setResult} />
+      <ResultCard result={result} />
+    </div>
+  );
+}
+
+export default Home;
